6ccecab5c34915385a858ba8f9dbaa16cf824394
Yasmine Abulhab <yabulhab@gmail.com>
2016-05-22 15:31:54 -0400

    set up templates, set up controllers for menu and home, fixed CSS

--- script.js
+++ script.js
@@ -1,1 +1,23 @@
+var lovettApp = angular.module('lovettApp', []);
+
 console.log("Hello, world!");
+
+// start menu controller 
+lovettApp.controller("MenuCtrl", function($scope) {
+
+
+	$scope.visibleMenu = false;
+
+	$scope.toggleFunc = function() {
+		$scope.visibleMenu = !$scope.visibleMenu;
+		console.log("I'm toggling");
+	}
+
+});
+
+//start Home controller
+
+lovettApp.controller("HomeCtrl", function($scope) {
+
+
+});
