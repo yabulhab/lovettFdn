73ab9fb46ba8235206b98a8b8afff874d8edebfd
Yasmine Abulhab <yabulhab@gmail.com>
2016-06-11 14:50:28 -0400

    fixed grid issue, animated CSS hamburger

--- script.js
+++ script.js
@@ -5,13 +5,22 @@ console.log("Hello, world!");
 // start menu controller 
 lovettApp.controller("MenuCtrl", function($scope) {
 
-
 	$scope.visibleMenu = false;
+	$scope.hamburger = angular.element(document.querySelectorAll("#s0, #s1, #s2"));
+	//console.log($scope.hamburger);
 
 	$scope.toggleFunc = function() {
+		
 		$scope.visibleMenu = !$scope.visibleMenu;
 		console.log("I'm toggling");
+		($scope.hamburger).toggleClass("open");
+		//console.log(hamburger);
+	
 	}
+			
+		// $scope.open = !$scope.open;
+		// console.log("menu toggle??")
+
 
 });
 
