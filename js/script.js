a09fa9f250dcb788d9e47c1aeb5c3ff66439f45c
Yasmine Abulhab <yabulhab@gmail.com>
2016-09-17 14:15:58 -0400

    updated tiles and created dropdown

--- js/script.js
+++ js/script.js
@@ -7,6 +7,7 @@ lovettApp.controller("MenuCtrl", function($scope) {
 
 	$scope.visibleMenu = false;
 	$scope.hamburger = angular.element(document.querySelectorAll("#s0, #s1, #s2"));
+	$scope.menuWord = angular.element(document.querySelector("#menuWord"));
 	//console.log($scope.hamburger);
 
 	$scope.toggleFunc = function() {
@@ -14,19 +15,19 @@ lovettApp.controller("MenuCtrl", function($scope) {
 		$scope.visibleMenu = !$scope.visibleMenu;
 		console.log("I'm toggling");
 		($scope.hamburger).toggleClass("open");
+		($scope.menuWord).toggleClass("open");
 		//console.log(hamburger);
 	
+	}			
+
+
+});
+
+function openCat(cat){
+	var i; 
+	var x = document.getElementsByClassName("successCat");
+	for (i=0; i<x.length; i++) {
+		x[i].style.display="none";
 	}
-			
-		// $scope.open = !$scope.open;
-		// console.log("menu toggle??")
-
-
-});
-
-//start Home controller
-
-lovettApp.controller("HomeCtrl", function($scope) {
-
-
-});
+	document.getElementById(cat).style.display="block";
+}
