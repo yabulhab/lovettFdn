017f920e9d8eaf14788c74fa5a2ac4230ac7fa39
Yasmine Abulhab <yabulhab@gmail.com>
2017-03-31 11:36:41 -0400

    redid navigation, cleaned up CSS

--- js/script.js
+++ js/script.js
@@ -1,69 +1,61 @@
 var lovettApp = angular.module('lovettApp', []);
 
-console.log("Hello, world!");
 
-
-
-// start menu controller 
-	lovettApp.controller("MenuCtrl", function($scope) {
-
-	$scope.visibleMenu = false;
-	$scope.hamburger = angular.element(document.querySelectorAll("#s0, #s1, #s2"));
-	$scope.menuWord = angular.element(document.querySelector("#menuWord"));
-	$scope.body = angular.element(document.querySelector("body"));
-	//we need this for when page loads to get the initial value of the window width so that 
-	//we have either a true or a false on load instead of waiting for a click
-	$scope.mobile = window.innerWidth < 768 ?  true : false ;
-	//console.log(window.innerWidth);
-	//console.log($scope.hamburger);
-
-
+lovettApp.controller("homeCtrl", function($scope) {
 //this function is listening for the size of the window and doing an appropriate true: false situation
 	$(window).resize(function(){
-	    console.log(window.innerWidth);
-	    $scope.mobile = window.innerWidth < 768 ?  true : false ;
-	    $scope.$apply(function(){
-
-	    });
+		console.log(window.innerWidth);
+		$scope.mobile = window.innerWidth < 768 ?  true : false ;
+		$scope.$apply(function(){
+		});
 	});
-
-	$scope.toggleFunc = function() {
-		
-		$scope.visibleMenu = !$scope.visibleMenu;
-		console.log("I'm toggling");
-		($scope.hamburger).toggleClass("open");
-		($scope.menuWord).toggleClass("open");
-		console.log($scope.body.style)
-		console.log($scope.body) 
-		//$scope.body[0].style.overflow = "hidden" ;
-		//console.log(hamburger);
-
-		// if ($scope.visibleMenu) {
-		// 	$scope.body[0].style.overflow = "hidden" ;
-		// } else {
-		// 	$scope.body[0].style.overflow = "scroll" ;
-		// };
-
-		$scope.body[0].style.overflow = $scope.visibleMenu ? "hidden" : "scroll";
-	
-	}
-
-
 });
 
+lovettApp.controller("MenuCtrl", function($scope) {
 
+		$scope.burger = angular.element(document.querySelectorAll('#s0, #s1, #s2'));
+		$scope.openMenu = function() {
+			document.getElementById("myMenu").style.width = "360px";
+			document.getElementById("menuWord").style.color = "white";
+			document.getElementById("bodyOverlay").style.display = "block";
+			($scope.burger).addClass('open');
+			console.log("i'm open");
+		}	
+		$scope.closeMenu = function(){
+			document.getElementById("myMenu").style.width = "0";
+			document.body.style.backgroundColor = "white";
+			document.getElementById("menuWord").style.color = "black";
+			document.getElementById("bodyOverlay").style.display = "none";
+			($scope.burger).removeClass('open');
+			console.log("i'm closed");
+		}
 
+		var triggerClicked = false;
+		$scope.triggerClick = function() {
+			$scope[triggerClicked ? "closeMenu" : "openMenu"]();
+			triggerClicked = !triggerClicked;
+			console.log("triggerClick");
+		};
 
-// 2.0
+		$("#bodyOverlay").on('click', function(event) {
+			$scope.triggerClick();
+			console.log("the overlay has been clicked");		
+		});
 
-// function openCat(cat){
-// 	var i; 
-// 	var x = document.getElementsByClassName("successCat");
-// 	for (i=0; i<x.length; i++) {
-// 		x[i].style.display="none";
-// 	}
-// 	document.getElementById(cat).style.display="block";
-// }
+		//adapted from Nick Ciliak's fade out on scroll https://codepen.io/nickcil/pen/sfutl
+		$(window).scroll(function(){
+			$("#menuWord").css("opacity", 1 -
+				$(window).scrollTop()/250);
+		});
+});	
+
+// lovettApp.controller("LogoCtrl", function($scope) {
+
+// });
+
+var scrollDown = function(){
+	$('html, body').animate({scrollTop: $('#about-mission').offset().top}, 100);
+};
 
 
 
